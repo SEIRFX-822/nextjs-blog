b0a368f0c2ea3c0552703d1c3453568786f948f9
Rome Bell <romebell5@gmail.com>
2023-12-02 14:09:38 -0800

    feat: modify home page

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,49 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home() { // exporting the function Home
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Rome's Engineering Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to my blog!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Get started by reading the blog.
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>First Blog &rarr;</h3>
+            <p>Learn about my engineering journey</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Second Blog &rarr;</h3>
+            <p>Learn about React</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Third Blog &rarr;</h3>
+            <p>Discover all the cool things about MongoDB.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Fourth Blog &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              My new job pays crazy good.
             </p>
           </a>
         </div>
